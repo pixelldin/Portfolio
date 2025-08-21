d9adc44a295d6ee5e0cc6a10f9b806d0717cdac9
Cheggin <79483996+Cheggin@users.noreply.github.com>
2025-08-21 10:22:33 -0700

    oops use react not nextjs

--- Portfolio/src/main.tsx
+++ Portfolio/src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { Analytics } from '@vercel/analytics/react';
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
